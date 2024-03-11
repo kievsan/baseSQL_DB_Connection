21feaeaa1f2c868924266f7a90bda5c285200d09
kievskiy <knhel7@mail.ru>
2024-03-11 10:47:12 +0300

    task2. liquibase

--- src/main/resources/db/changelog/migrations/001-schema.sql
+++ src/main/resources/db/changelog/migrations/001-schema.sql
@@ -1,12 +1,18 @@
-create schema db3 authorization postgres;
+create schema if not exists orders.db3 authorization postgres;
 
 create table if not exists db3.customers (
     id serial primary key ,
     name varchar(20) not null ,
     surname varchar(30) not null ,
     age int not null ,
-    phone_number bigint
+    phone_number varchar(12)
 );
+
+insert into db3.customers (name, surname, age, phone_number)
+VALUES ('Вася', 'Пупкин', 20, 9614005530),
+       ('Сеня', 'Гоцман', 43, 9223334455),
+       ('alexey', 'Ред', 30, 9376584421),
+       ('Serg', 'Green', 33, 9216320122);
 
 create table if not exists db3.orders (
     id serial primary key ,
@@ -15,3 +21,10 @@ create table if not exists db3.orders (
     amount decimal(10,2) not null,
     customer_id int references db3.customers (id)
 );
+
+insert into db3.orders (product_name, amount, customer_id)
+VALUES ('телевизор Sony', 56500.00, 1),
+       ('утюг Scarlett', 4700.00, 2),
+       ('телевизор Samsung', 42000.00, 3),
+       ('куртка', 3370.00, 3),
+       ('джинсы', 5030.00, 4);

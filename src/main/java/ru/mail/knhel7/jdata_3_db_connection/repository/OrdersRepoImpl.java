933b8d764b1ff9cf59aef8f7b6b2f7911b854d62
kievskiy <knhel7@mail.ru>
2024-03-12 07:42:16 +0300

    task2. hibernate jpa

--- src/main/java/ru/mail/knhel7/jdata_3_db_connection/repository/OrdersRepoImpl.java
+++ src/main/java/ru/mail/knhel7/jdata_3_db_connection/repository/OrdersRepoImpl.java
@@ -1,5 +1,7 @@
 package ru.mail.knhel7.jdata_3_db_connection.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -14,20 +16,30 @@ import java.util.Map;
 @Repository
 public class OrdersRepoImpl implements OrdersRepo {
 
-    private final String sqlQuery = SqlReader.read("./sql_queries/namedQuery01_CustomerNameByProductName.sql");
-    private final NamedParameterJdbcTemplate jdbc;
+    @PersistenceContext
+    private EntityManager manager;
 
-    // Внедрение bean, созданного Spring Boot для работы с сохраненными данными:
-    public OrdersRepoImpl(NamedParameterJdbcTemplate jdbc) {
-        this.jdbc = jdbc;
-    }
+//    private final String sqlQuery = SqlReader.read("./sql_queries/namedQuery01_CustomerNameByProductName.sql");
+//    private final NamedParameterJdbcTemplate jdbc;
+//
+//    // Внедрение bean, созданного Spring Boot для работы с сохраненными данными:
+//    public OrdersRepoImpl(NamedParameterJdbcTemplate jdbc) {
+//        this.jdbc = jdbc;
+//    }
+//
+//    @Override
+//    public List<Product> getProductName(String name) {
+//        RowMapper<Product> productRowMapper = (resultSet, i) -> new Product(resultSet.getString("product_name"));
+//        Map<String, Object> paramMap = new HashMap<>();
+//        paramMap.put("name", name);
+//        return jdbc.query(sqlQuery, paramMap, productRowMapper);
+//    }
 
     @Override
     public List<Product> getProductName(String name) {
-        RowMapper<Product> productRowMapper = (resultSet, i) -> new Product(resultSet.getString("product_name"));
-        Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("name", name);
-        return jdbc.query(sqlQuery, paramMap, productRowMapper);
+
+
+        return null;
     }
 
 }
